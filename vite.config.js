9e8126db850aced037bb5418e0371cdc8d51286c
Jada Williams <jada.23.01@gmail.com>
2025-11-16 17:25:15 -0500

    chore: restore Vite config with base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/venus-saver/',  //  changed this
+  base: '/venus-saver/',
   plugins: [react()],
 })
